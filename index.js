311e534b6ee95d6eaa2b4fc124cc75c55d060a0b
krishna1674 <vijayakrishnaalla@gmail.com>
2024-07-21 09:24:27 +0530

    modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Brandname = require('./model');
+const BrandName = require('./model');
 const app= express();
 
 app.use(express.json())
@@ -11,16 +11,16 @@ mongoose.connect('mongodb+srv://vijayakrishnaalla:1674@first1.gttqspo.mongodb.ne
 //     res.send('<h2>hello world</h2>')
 // })
 app.post('/addbrands',async (req,res)=>{
-    const {brandname} = req.body ;
+    const {brandname} = req.body;
     try{
-        const newData = new Brandname({brandname});
+        const newData = new BrandName({brandname});
         await newData.save();
-        return res.json(await Brandname.find())
+        return res.json(await BrandName.find());
     }
     catch(err){
         console.log(err.message);
     }
 })
 
-app.listen(5500,()=>console.log('server running'))
+app.listen(8080,()=>console.log('server running'))
 
